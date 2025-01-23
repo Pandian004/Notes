0f63127fe23b37980794a0ba0e34065c20fad97a
Pandian004 <pandiaan1969@gamil.com>
2025-01-23 09:59:46 +0530

    Add a export button in navbar, Frontend

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
-// import {Link} from 'react-router-dom';
-import Dropdown from'./input/DropDown.js';
+import {Link} from 'react-router-dom';
+// import Dropdown from'./input/DropDown.js';
+import { BiExport } from "react-icons/bi";
 
 const Navbar = ({isOpen}) => {
   return (
@@ -8,15 +9,18 @@ const Navbar = ({isOpen}) => {
       <div className=''>
         <p className='text-3xl'>Notes</p>
       </div>
-      <div className=''>
-        {/* <div className='space-x-4'>
+      <div className='flex space-x-10 justify-center items-center'>
+        <div className='flex space-x-2 justify-center py-2 px-4 bg-slate-50 rounded-xl cursor-pointer hover:bg-black hover:text-white transition ease-in-out duration-300'>
+          <p className='text-xl'><BiExport /></p>
+          <button>Export as PDF</button>
+        </div>
+        <div className='space-x-4'>
           <Link to = '/login'><button className='p-2 px-3 rounded-3xl bg-slate-100'>Login</button></Link>
           <Link to = '/signup'><button className='p-2 rounded-3xl bg-black text-white'>Signup</button></Link>  
-        </div> */}
-        <div className='flex space-x-2 justify-center items-center'>
-          {/* <p className='text-lg py-2 px-4 rounded-full bg-slate-100 cursor-default'>D</p> */}
-          <Dropdown />
         </div>
+        {/* <div className=''>
+          <Dropdown />
+        </div> */}
       </div>
     </div>
   )
